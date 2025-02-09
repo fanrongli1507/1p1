48b32b451ff435fad3f7a6882e94c506c7bb23a1
hx-the-great <fanrongli1507@gmail.com>
2025-02-09 12:48:37 +0800

    Announcements Styling

--- scripts/announcements.js
+++ scripts/announcements.js
@@ -9,10 +9,10 @@ async function renderAnnouncements() {
     .map((value, index) => {
       const bgClass = index % 2 === 0 ? "black-bg" : "white-bg";
       return `<div class="${bgClass}">
-        <div>${value.name}</div>
-        <div>${value.content}</div>
-        <div>${new Date(value["date-created"]).toLocaleString()}</div>
-        <div>${new Date(value["deadline"]).toLocaleString()}</div>
+        <div class="name">${value.name}</div>
+        <div class="content">${value.content}</div>
+        <div class="date-created">Date created: ${new Date(value["date-created"]).toLocaleString()}</div>
+        <div class="deadline">Deadline: ${new Date(value["deadline"]).toLocaleString()}</div>
       </div>`;
     })
     .join("");
