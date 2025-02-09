d6c49265b8a7b1ffa741cc15756900fd3d5b24ef
hx-the-great <fanrongli1507@gmail.com>
2025-02-09 20:56:06 +0800

    Chenged something

--- scripts/create.js
+++ scripts/create.js
@@ -1,5 +1,3 @@
-
-
 document.querySelector(".js-submit").addEventListener("click", async () => {
   const name = document.querySelector(".js-name").value;
   const content = document.querySelector(".js-content").value;
@@ -11,23 +9,27 @@ document.querySelector(".js-submit").addEventListener("click", async () => {
     "date-created": new Date(),
     "deadline": deadline,
   };
-
-  try {
-    const response = await fetch("https://onep1-announcements.onrender.com/announcements", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(announcement),
-    });
-
-    const data = await response.json();
-    console.log(data);
-    alert('Sent successfully.');
-    document.querySelector(".js-name").value ='';
-    document.querySelector(".js-content").value = '';
-    document.querySelector(".js-deadline").value ='';
-  } catch (error) {
-    console.error("Error:", error);
+  if (name && content && deadline) {
+    alert('Please fill up all areas.')
+  } else {
+    try {
+      const response = await fetch("https://onep1-announcements.onrender.com/announcements", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(announcement),
+      });
+  
+      const data = await response.json();
+      console.log(data);
+      alert('Sent successfully.');
+      // Clear the form after submission
+      document.querySelector(".js-name").value = '';
+      document.querySelector(".js-content").value = '';
+      document.querySelector(".js-deadline").value = '';
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
 });
