9cf1c25231f903e80afd23bd4fe71c85b69ca71d
hx-the-great <fanrongli1507@gmail.com>
2025-02-20 15:43:42 +0800

    Fixed create.js

--- scripts/create.js
+++ scripts/create.js
@@ -4,6 +4,10 @@ document.querySelector(".js-submit").addEventListener("click", async () => {
   const deadline = `${document.querySelector(".js-deadline").value}T15:59:59`
   ;
 
+
+if (!name || !content || deadline === 'T15:59:59') {
+  alert('Please fill up all areas.');
+} else {
   const announcement = {
     "name": name,
     "content": content,
@@ -11,9 +15,8 @@ document.querySelector(".js-submit").addEventListener("click", async () => {
     "deadline": deadline,
   };
 
-  if (!name || !content || !deadline) {
-    alert('Please fill up all areas.');
-  } else {
+
+
     try {
       const response = await fetch("https://onep1-announcements.onrender.com/announcements", {
         method: "POST",
@@ -33,5 +36,5 @@ document.querySelector(".js-submit").addEventListener("click", async () => {
     } catch (error) {
       console.error("Error:", error);
     }
-  }
+}
 });
